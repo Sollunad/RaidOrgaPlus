21a34ca623ccbca7f9374334b0163dc647c21cfa
Daniel <danielklaus@hotmail.de>
2018-12-20 19:43:17 +0100

    db connector in frontend? idk yet

---
+++ frontend/src/services/db/connector.js
@@ -0,0 +1,32 @@
+const config = require("./config.json");
+const sql = require('mysql');
+
+exports.query = query;
+exports.queryV = queryV;
+
+function query(command) {
+  const con = sql.createConnection(config);
+  return new Promise( function(resolve, reject) {
+    con.query(command, (err,rows) => {
+      if (err) return reject(err);
+      resolve(rows);
+    });
+    con.end();
+  });
+}
+
+function queryV(command, values) {
+    return new Promise( (resolve, reject) => {
+      con.query(command, values, (err,rows) => {
+        if (err) return reject(err);
+        resolve(rows);
+      });
+      con.end();
+    });
+}
+
+async function test() {
+  console.log(await query('SELECT * from Spieler'));
+}
+
+test();
