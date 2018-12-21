bbf49f40c5a6b783e59da0491160b0c0714e3a4d
Daniel <danielklaus@hotmail.de>
2018-12-21 16:33:24 +0100

    actually using server now instead of localhost

--- frontend/src/services/raids.js
+++ frontend/src/services/raids.js
@@ -3,13 +3,13 @@ const sf = require('snekfetch');
 export default { listForPlayer, give };
 
 async function listForPlayer(userId) {
-    const url = 'http://localhost:3000/raids?user=' + userId;
+    const url = 'http://sv.sollunad.de:3000/raids?user=' + userId;
     const response = await sf.get(url);
     return response.body;
 }
 
 async function give(raidId) {
-    const url = 'http://localhost:3000/raids?raid=' + raidId;
+    const url = 'http://sv.sollunad.de:3000/raids?raid=' + raidId;
     const response = await sf.get(url);
     return response.body[0];
 }
