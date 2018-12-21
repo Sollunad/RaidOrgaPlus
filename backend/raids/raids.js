49aa5a8688036e6b90293b9774080fee01cce7cd
Daniel <danielklaus@hotmail.de>
2018-12-21 16:04:36 +0100

    function to give a single raid from id

--- backend/raids/raids.js
+++ backend/raids/raids.js
@@ -1,9 +1,19 @@
 const db = require('../db/connector.js');
 
-exports.listForPlayer = listForPlayer;
+exports.listForPlayer = listForPlayerId;
+exports.give = giveForRaidId;
 
-async function listForPlayer(userId) {
-    const stmt = `SELECT Raid.id, Raid.name, Raid.icon FROM Spieler JOIN Spieler_Raid ON Spieler.id = Spieler_Raid.fk_spieler JOIN Raid ON Raid.id = Spieler_Raid.fk_raid WHERE Spieler.id = ${userId}`;
+async function listForPlayerId(userId) {
+    const stmt = `SELECT Raid.id, Raid.name, Raid.icon, Spieler_Raid.role FROM Spieler JOIN Spieler_Raid ON Spieler.id = Spieler_Raid.fk_spieler JOIN Raid ON Raid.id = Spieler_Raid.fk_raid WHERE Spieler.id = ${userId}`;
+    try {
+        return await db.query(stmt);
+    } catch(e) {
+        throw e;
+    }
+}
+
+async function giveForRaidId(raidId) {
+    const stmt = `SELECT * FROM Raid WHERE id = ${raidId}`;
     try {
         return await db.query(stmt);
     } catch(e) {
--- frontend/src/services/raids.js
+++ frontend/src/services/raids.js
@@ -1,9 +1,15 @@
 const sf = require('snekfetch');
 
-export default { listForPlayer };
+export default { listForPlayer, give };
 
 async function listForPlayer(userId) {
     const url = 'http://localhost:3000/raids?user=' + userId;
     const response = await sf.get(url);
     return response.body;
+}
+
+async function give(raidId) {
+    const url = 'http://localhost:3000/raids?raid=' + raidId;
+    const response = await sf.get(url);
+    return response.body[0];
 }
