be03ecbb2babec16aa9aaded43b2cab2eaf2aef9
Daniel <danielklaus@hotmail.de>
2018-12-21 16:43:18 +0100

    added config for backend ip (localhost / server)

--- frontend/src/services/raids.js
+++ frontend/src/services/raids.js
@@ -1,15 +1,16 @@
 const sf = require('snekfetch');
+const config = require('./config.json');
 
 export default { listForPlayer, give };
 
 async function listForPlayer(userId) {
-    const url = 'http://sv.sollunad.de:3000/raids?user=' + userId;
+    const url = config.url + 'raids?user=' + userId;
     const response = await sf.get(url);
     return response.body;
 }
 
 async function give(raidId) {
-    const url = 'http://sv.sollunad.de:3000/raids?raid=' + raidId;
+    const url = config.url + 'raids?raid=' + raidId;
     const response = await sf.get(url);
     return response.body[0];
 }
