48271fe09dbb4354b486e03498f72a878a4dea11
Daniel <danielklaus@hotmail.de>
2018-12-24 16:30:42 +0100

    move routes to separate file

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -18,33 +18,16 @@
   import MenuComp from './components/MenuComp.vue';
   import FooterComp from './components/FooterComp';
 
-  import MeineRaidsPage from './pages/MeineRaidsPage.vue';
-  import RaidDashboardPage from "./pages/RaidDashboardPage";
-  import TerminePage from "./pages/TerminePage";
-  import AufstellungPage from "./pages/AufstellungPage";
-  import ArchivPage from "./pages/ArchivPage";
-  import ProfilPage from "./pages/ProfilPage";
-  import SkillsPage from "./pages/SkillsPage";
-  import EinstellungenPage from "./pages/EinstellungenPage";
-
   import session from './services/session.js';
   import MainPage from "./pages/MainPage";
   import LoginRegisterPage from "./pages/LoginRegisterPage";
 
-  const routes = [
-      { path: '/raids', component: MeineRaidsPage },
-      { path: '/raid', component: RaidDashboardPage },
-      { path: '/termine', component: TerminePage },
-      { path: '/aufstellung', component: AufstellungPage },
-      { path: '/archiv', component: ArchivPage },
-      { path: '/profil', component: ProfilPage },
-      { path: '/skills', component: SkillsPage },
-      { path: '/einstellungen', component: EinstellungenPage }
-  ];
+  import routes from './routes.js';
 
   const router = new VueRouter({routes});
   Vue.use(VueRouter);
   Vue.use(VueAsync);
+
   export default {
     components: {
       LoginRegisterPage,
---
+++ frontend/src/routes.js
@@ -0,0 +1,21 @@
+import MeineRaidsPage from './pages/MeineRaidsPage.vue';
+import RaidDashboardPage from "./pages/RaidDashboardPage";
+import TerminePage from "./pages/TerminePage";
+import AufstellungPage from "./pages/AufstellungPage";
+import ArchivPage from "./pages/ArchivPage";
+import ProfilPage from "./pages/ProfilPage";
+import SkillsPage from "./pages/SkillsPage";
+import EinstellungenPage from "./pages/EinstellungenPage";
+
+const routes = [
+    { path: '/raids', component: MeineRaidsPage },
+    { path: '/raid', component: RaidDashboardPage },
+    { path: '/termine', component: TerminePage },
+    { path: '/aufstellung', component: AufstellungPage },
+    { path: '/archiv', component: ArchivPage },
+    { path: '/profil', component: ProfilPage },
+    { path: '/skills', component: SkillsPage },
+    { path: '/einstellungen', component: EinstellungenPage }
+];
+
+export default routes;
