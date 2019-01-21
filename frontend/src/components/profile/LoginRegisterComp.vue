b57df1f8e6cc169e5022ca06e840a41db1e4f8fa
Daniel <danielklaus@hotmail.de>
2019-01-21 19:07:30 +0100

    #38 fix account name regex

--- frontend/src/components/profile/LoginRegisterComp.vue
+++ frontend/src/components/profile/LoginRegisterComp.vue
@@ -70,7 +70,7 @@
             accName: '',
             accNameRules: [
                 v => !!v || 'Bitte gib deinen Accountnamen an',
-                v => /\w+.\d{4}/.test(v) || 'Bitte gib einen gültigen Accountnamen an',
+                v => /^[a-zA-Z]+.\d{4}$/.test(v) || 'Bitte gib einen gültigen Accountnamen an',
             ],
             name: '',
             nameRules: [
