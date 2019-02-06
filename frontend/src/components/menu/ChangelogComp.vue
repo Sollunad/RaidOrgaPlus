eb3cd06368071571f9769f4e2f8f89bf0cc2c109
Daniel <danielklaus@hotmail.de>
2019-02-06 22:44:52 +0100

    #89 Changelog

---
+++ frontend/src/components/menu/ChangelogComp.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="changelog">
+        <v-expansion-panel>
+            <v-expansion-panel-content
+                    v-for="(version, index) in versions"
+                    :key="index"
+            >
+                <div slot="header">v{{ version.no }} - {{ version.release }}</div>
+                <v-card color="blue-grey darken-2">
+                    <v-card-text>
+                        <ul>
+                            <li v-for="feature in version.features" :key="feature">
+                                {{ feature }}
+                            </li>
+                        </ul>
+                        <v-expansion-panel>
+                            <v-expansion-panel-content
+                                    v-for="(subversion, index) in version.subversions"
+                                    :key="index"
+                            >
+                                <div slot="header">v{{ subversion.no }} - {{ subversion.release }}</div>
+                                <v-card color="blue-grey darken-2">
+                                    <v-card-text>
+                                        <ul>
+                                            <li v-for="feature in subversion.features" :key="feature">
+                                                {{ feature }}
+                                            </li>
+                                        </ul>
+                                    </v-card-text>
+                                </v-card>
+                            </v-expansion-panel-content>
+                        </v-expansion-panel>
+                    </v-card-text>
+                </v-card>
+            </v-expansion-panel-content>
+        </v-expansion-panel>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ChangelogComp",
+        data: () => ({
+            versions: [
+                {no: '1.0', release: '24.02.2019',
+                    features: [
+                        'Spielerliste',
+                        'Termine / Archiv',
+                        'Blankos',
+                        'Meine Builds',
+                        'Weekly Progress',
+                        'LI / LD',
+                    ], subversions: [
+
+                    ]},
+                {no: '1.1', release: 'März 2019',
+                    features: [
+                        'Lieutenants ernennen',
+                        'Builds in Spielerliste filtern',
+                        'Eigene Skillcard',
+                        'Öffentliche Profile',
+                        'Profilbilder',
+                        'Fortlaufende Termine',
+                        'Raid-Drops',
+                    ]},
+                {no: '1.2', release: 'April 2019',
+                    features: [
+                        'Logs hochladen und anzeigen',
+                        'Videos verlinken',
+                        'Statistiken',
+                        'Kill Proofs',
+                    ]},
+            ],
+        })
+    }
+</script>
+
+<style scoped>
+    .changelog {
+        background-color: #444444;
+    }
+
+    .version:not(:last-child) {
+        margin-bottom: 10px;
+    }
+
+    .text {
+        margin-bottom: 10px;
+    }
+
+</style>
--- frontend/src/components/menu/FooterComp.vue
+++ frontend/src/components/menu/FooterComp.vue
@@ -1,33 +1,52 @@
 <template>
     <v-footer app fixed>
-        <span class="footerText">{{ footerText }}</span>
-        <a class="feedback" target="_newtab" href="https://goo.gl/forms/VSlcFr3DdhK0flDe2">Feedback?</a>
+        <div class="footer">
+            <v-dialog width="600">
+                <span class="version" slot="activator">v{{ versionNo }}</span>
+                <ChangelogComp></ChangelogComp>
+            </v-dialog>
+            <span class="footerText">Made with ❤ by Sollunad.9780</span>
+            <a class="feedback" target="_newtab" href="https://goo.gl/forms/VSlcFr3DdhK0flDe2">Feedback?</a>
+        </div>
     </v-footer>
 </template>
 
 <script>
+    import ChangelogComp from "./ChangelogComp";
     export default {
         name: "FooterComp",
+        components: {ChangelogComp},
         data: () => ({
             versionNo: '0.1',
-            versionTitle: 'alpha'
         }),
         computed: {
             footerText: function() {
-                return `v${this.versionNo} (${this.versionTitle}) - Made with ❤ by Sollunad.9780`;
+                return `v${this.versionNo} (${this.versionTitle})`;
             }
         }
     }
 </script>
 
 <style scoped>
+    .footer {
+        text-align: center;
+        width: 100%;
+    }
+
+    .footerText {
+        margin-left: 10px;
+    }
+
     .feedback {
         margin-left: 10px;
-        text-decoration: underline;
+        text-decoration: none;
         color: inherit;
     }
 
-    .feedback:hover {
+    .version {
+    }
+
+    .version:hover {
         cursor: pointer;
     }
 </style>
