8f15fe780089ce64df4b4d010cf2a67b4436785b
Daniel <danielklaus@hotmail.de>
2019-02-25 16:40:57 +0100

    #137 Cookies auf 30 Tage gesetzt

--- frontend/src/services/util/cookies.js
+++ frontend/src/services/util/cookies.js
@@ -2,7 +2,7 @@ export default { setCookie, getCookie, deleteCookie };
 
 function setCookie(cname, cvalue) {
     const d = new Date();
-    d.setTime(d.getTime() + (7 * 24 * 60 * 60 * 1000));
+    d.setTime(d.getTime() + (30 * 24 * 60 * 60 * 1000));
     const expires = "expires="+d.toUTCString();
     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
