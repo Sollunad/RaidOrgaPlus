931bfb4f962c957f74f1f0ec920cb4874a4183e2
Daniel <danielklaus@hotmail.de>
2019-03-02 11:33:18 +0100

    #146 deploy scripts

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,9 +1,11 @@
+#!/bin/sh
 git reset --hard
 git pull
 npm run install
+npm run serve
 
 screen -S backend-dev -X quit
 screen -d -m -S backend-dev bash -c 'cd backend && nodejs app.js'
 
 screen -S frontend-dev -X quit
-screen -d -m -S frontend-dev bash -c 'npm run serve'
+screen -d -m -S frontend-dev bash -c 'cd frontend && serve dist'
--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
+#!/bin/sh
 git reset --hard
 git pull
 npm run install
 npm run build
+
 rm -r /var/www/html
 mv frontend/dist /var/www/html
 service apache2 restart
