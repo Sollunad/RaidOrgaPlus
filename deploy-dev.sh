348dbf1fbb5da7701f61e1c9536f6038db9f5b87
Daniel <danielklaus@hotmail.de>
2019-03-02 11:41:10 +0100

    #146 deploy scripts

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -8,4 +8,4 @@ screen -S backend-dev -X quit
 screen -d -m -S backend-dev bash -c 'cd backend && nodejs app.js'
 
 screen -S frontend-dev -X quit
-screen -d -m -S frontend-dev bash -c 'cd frontend && serve dist'
+screen -d -m -S frontend-dev bash -c 'cd frontend && serve -l 5001 dist'
