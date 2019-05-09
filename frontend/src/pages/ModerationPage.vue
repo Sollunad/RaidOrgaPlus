d4266e7c5eb13ea983535ba224eb7be8119382df
Daniel <danielklaus@hotmail.de>
2019-05-09 17:08:40 +0200

    #167 create moderation page

---
+++ frontend/src/pages/ModerationPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div v-if="user.role > 0">
+            Moderation!
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ModerationPage",
+        props: ['user']
+    }
+</script>
+
+<style scoped>
+
+</style>
