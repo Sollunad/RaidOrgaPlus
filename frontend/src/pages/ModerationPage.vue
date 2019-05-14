31815f743cacf02e446b424093a066be690b7bd1
Daniel <danielklaus@hotmail.de>
2019-05-14 17:22:31 +0200

    #168 Moderation Spieler anzeigen (wip)

--- frontend/src/pages/ModerationPage.vue
+++ frontend/src/pages/ModerationPage.vue
@@ -1,18 +1,22 @@
 <template>
-    <div>
+    <div class="moderation">
         <div v-if="user.role > 0">
-            Moderation!
+            <ModUserListComp></ModUserListComp>
         </div>
     </div>
 </template>
 
 <script>
+    import ModUserListComp from "../components/moderation/ModUserListComp";
     export default {
         name: "ModerationPage",
+        components: {ModUserListComp},
         props: ['user']
     }
 </script>
 
 <style scoped>
-
+    .moderation {
+        margin: 3%;
+    }
 </style>
