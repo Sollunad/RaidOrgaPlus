44cfef2fe14b3d8d87b98816c23c072e523dc7db
Daniel <danielklaus@hotmail.de>
2019-07-05 15:08:40 +0200

    #193 try ubuntu local parser

--- backend/reports/parser.js
+++ backend/reports/parser.js
@@ -2,13 +2,14 @@ const fs = require('fs');
 const exec = require('child_process').execFile;
 
 const PARSER_PATH = 'reports/Parser/GuildWars2EliteInsights.exe';
+const COMMAND = `mono ${PARSER_PATH} -p`;
 
 exports.parse = parse;
 
 async function parse(filepath) {
     const id = filepath.split('/')[3].split('.')[0];
     return new Promise((resolve, reject) => {
-        exec(PARSER_PATH, [filepath], function(err, data) {
+        exec(COMMAND, [filepath], function(err, data) {
             console.log(id);
             fs.unlinkSync(filepath);
             if (err) {
