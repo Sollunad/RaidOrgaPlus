6472f8f4688268dfdb8d5cf31d0d02f1dd9bba16
Daniel <danielklaus@hotmail.de>
2019-07-11 14:47:04 +0200

    #218 show ingame gilde in moderation

--- backend/endpoints/moderation/controller.js
+++ backend/endpoints/moderation/controller.js
@@ -1,6 +1,7 @@
 const _roles = require('../../authentication/role');
 const _users = require('./users');
 const _discord = require('../../discord/users');
+const _guild = require('../../gw2api/guilds');
 
 module.exports = [
     {function: getUsers, path: '/users', method: 'get', authed: true}
@@ -11,10 +12,15 @@ async function getUsers(req, authentication) {
     if (role > 0) {
         const users = await _users.getUsers();
         const discordUsers = await _discord.getAllUsers();
+        const guildUsers = await _guild.getUsers();
         for (const user of users) {
             const discordUser = _discord.findUser(user, discordUsers);
+            const guildUser = _guild.findUser(user, guildUsers);
             if (discordUser) {
                 user.discord = discordUser;
+            }
+            if (guildUser) {
+                user.guild = guildUser;
             }
         }
         return users;
