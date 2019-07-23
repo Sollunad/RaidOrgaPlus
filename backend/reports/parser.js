685d9d43ab896a67ae28c532c648f88a05016740
DanielDaKlaus <daniel.da.klaus@deutschebahn.com>
2019-07-23 14:40:44 +0200

    add config path to parser

--- backend/reports/parser.js
+++ backend/reports/parser.js
@@ -2,14 +2,14 @@ const fs = require('fs');
 const exec = require('child_process').execFile;
 
 const PARSER_PATH = 'reports/Parser/GuildWars2EliteInsights.exe';
-const COMMAND = `mono ${PARSER_PATH} -p`;
+const CONFIG_PATH = 'reports/Parser/GuildWars2EliteInsights.exe.config';
 
 exports.parse = parse;
 
 async function parse(filepath) {
     const id = filepath.split('/')[2].split('.')[0];
     return new Promise((resolve, reject) => {
-        exec('mono', [PARSER_PATH, '-p', filepath], function(err, data) {
+        exec('mono', [PARSER_PATH, '-c', CONFIG_PATH, '-p', filepath], function(err, data) {
             console.log(id);
             fs.unlinkSync(filepath);
             if (err) {
