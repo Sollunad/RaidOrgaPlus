f57fd781c466a7056cd53a9708c07d3a1d42bd45
DanielDaKlaus <daniel.da.klaus@deutschebahn.com>
2019-07-31 12:50:37 +0200

    #247 add comment for caching

--- backend/gw2api/items.js
+++ backend/gw2api/items.js
@@ -4,5 +4,6 @@ const api = apiclient();
 exports.getForId = getItemForId;
 
 async function getItemForId(id) {
+    // Kein extra Caching notwendig, macht der gw2api-client (24 Stunden Dauer)
     return await api.items().get(id);
 }
