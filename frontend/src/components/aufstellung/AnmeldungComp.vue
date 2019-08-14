76c27eaadf5c552d8bd79e2ce6d1bb2d33ba4f23
DanielDaKlaus <daniel.da.klaus@deutschebahn.com>
2019-08-14 12:00:24 +0200

    #250 add tooltips to anmelden buttons

--- frontend/src/components/aufstellung/AnmeldungComp.vue
+++ frontend/src/components/aufstellung/AnmeldungComp.vue
@@ -1,14 +1,23 @@
 <template>
     <v-btn-toggle v-model="buttonValue">
-        <v-btn flat color="green">
-            <v-icon>check_circle</v-icon>
-        </v-btn>
-        <v-btn flat color="yellow">
-            <v-icon>check_circle_outline</v-icon>
-        </v-btn>
-        <v-btn flat color="red">
-            <v-icon>cancel</v-icon>
-        </v-btn>
+        <v-tooltip bottom>
+            <v-btn flat color="green" slot="activator">
+                <v-icon>check_circle</v-icon>
+            </v-btn>
+            Ich nehme teil
+        </v-tooltip>
+        <v-tooltip bottom>
+            <v-btn flat color="yellow" slot="activator">
+                <v-icon>check_circle_outline</v-icon>
+            </v-btn>
+            Ich nehme vielleicht teil
+        </v-tooltip>
+        <v-tooltip bottom>
+            <v-btn flat color="red" slot="activator">
+                <v-icon>cancel</v-icon>
+            </v-btn>
+            Ich nehme nicht teil
+        </v-tooltip>
     </v-btn-toggle>
 </template>
 
