6cb7e9defbf22d9fcfb1970b413d0a3a9fc23ff3
Daniel <danielklaus@hotmail.de>
2019-09-03 13:09:25 +0200

    #306 fix missing players in moderation

--- backend/endpoints/moderation/users.js
+++ backend/endpoints/moderation/users.js
@@ -2,15 +2,18 @@ const db = require('../../db/connector.js');
 
 exports.getUsers = getUsers;
 
+const stmt = 'SELECT Spieler.id, Spieler.accname, Spieler.name, Spieler.lastActive, MAX(Termin.date) AS lastTermin FROM Spieler ' +
+    'JOIN Spieler_Termin ON Spieler_Termin.fk_spieler = Spieler.id ' +
+    'JOIN Termin ON Spieler_Termin.fk_termin = Termin.id ' +
+    'WHERE (Spieler_Termin.type = 0 OR Spieler_Termin.type = 1) ' +
+    'AND Spieler.id > 1 ' +
+    'GROUP BY id, accname, name, lastActive ' +
+    'UNION ' +
+    'SELECT Spieler.id, Spieler.accname, Spieler.name, Spieler.lastActive, NULL FROM Spieler ' +
+    'WHERE Spieler.id > 1 ' +
+    'AND Spieler.id NOT IN (SELECT DISTINCT fk_spieler FROM Spieler_Termin) ' +
+    'ORDER BY accname';
 async function getUsers() {
-    const stmt = 'SELECT Spieler.id, Spieler.accname, Spieler.name, Spieler.lastActive, MAX(Termin.date) AS lastTermin FROM Spieler ' +
-        'LEFT JOIN Spieler_Termin ON Spieler_Termin.fk_spieler = Spieler.id ' +
-        'LEFT JOIN Termin ON Spieler_Termin.fk_termin = Termin.id ' +
-        'WHERE (Spieler_Termin.type = 0 OR Spieler_Termin.type = 1 OR ISNULL(Spieler_Termin.type)) ' +
-        'AND (Termin.date < CURDATE() OR ISNULL(Termin.date)) ' +
-        'AND Spieler.id > 1 ' +
-        'GROUP BY Spieler.id, Spieler.accname, Spieler.name, Spieler.lastActive ' +
-        'ORDER BY Spieler.accname';
     try {
         return await db.query(stmt);
     } catch(e) {
