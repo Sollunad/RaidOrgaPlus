e74f24f2cecd1c2f5dfd0a92233bbcdae819ba8b
Daniel <danielklaus@hotmail.de>
2019-09-30 11:46:25 +0200

    #321 fix missing termine

--- backend/endpoints/kalender/termine.js
+++ backend/endpoints/kalender/termine.js
@@ -6,7 +6,7 @@ async function showTermineForNext7Days() {
     const stmt = 'SELECT Termin.id, Termin.date, Termin.time, Raid.name ' +
         'FROM Termin ' +
         'JOIN Raid ON Termin.fk_raid = Raid.id ' +
-        'WHERE Termin.date > CURDATE() AND Termin.date < CURDATE() + 7';
+        'WHERE Termin.date >= CURDATE() AND Termin.date < DATE_ADD(CURDATE(), INTERVAL 7 DAY)';
     try {
         return await db.query(stmt);
     } catch(e) {
