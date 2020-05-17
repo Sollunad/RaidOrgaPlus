38008bf929a9dccf9c21bc6ded07b9b12b3c4e39
Daniel Klaus <d@nielklaus.de>
2020-05-17 12:41:01 +0200

    #354 update build

--- backend/endpoints/build/controller.js
+++ backend/endpoints/build/controller.js
@@ -2,7 +2,7 @@ module.exports = [
     {function: checkBuildVersion, path: '', method: 'get', authed: false},
 ];
 
-const EXPECTED_BUILD = '2020_05_09';
+const EXPECTED_BUILD = '2020_05_17';
 
 async function checkBuildVersion(req, authentication) {
     const fe_build = req.query.build;
