101bd8a7fc688c5f592b842d7cf911b3a89b4f62
Daniel Klaus <d@nielklaus.de>
2020-05-18 13:23:25 +0200

    #387 hide archived termin in kalender

--- backend/endpoints/kalender/termine.js
+++ backend/endpoints/kalender/termine.js
@@ -6,7 +6,7 @@ async function showTermineForNext7Days() {
     const stmt = 'SELECT Termin.id, Termin.date, Termin.time, Termin.endtime, Raid.name ' +
         'FROM Termin ' +
         'JOIN Raid ON Termin.fk_raid = Raid.id ' +
-        'WHERE Termin.date >= CURDATE() AND Termin.date < DATE_ADD(CURDATE(), INTERVAL 7 DAY) ' +
+        'WHERE Termin.isArchived = 0 AND Termin.date >= CURDATE() AND Termin.date < DATE_ADD(CURDATE(), INTERVAL 7 DAY) ' +
         'ORDER BY Termin.date, Termin.time';
     try {
         return await db.query(stmt);
