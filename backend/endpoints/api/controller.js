a2294f9a5e4df493b4c899d7ee338268dfcfc061
ProfBits <53108880+ProfBits@users.noreply.github.com>
2020-11-19 10:22:49 +0100

    Fixed SQL Statements, reduced db calls to about 1/3

--- backend/endpoints/api/controller.js
+++ backend/endpoints/api/controller.js
@@ -14,8 +14,8 @@ async function setAufstellung(req, authentication) {
     const termin = req.body.body.terminId;
     const data = req.body.body.aufstellungen;
 
-    const stmt = 'SELECT count(*) AS count FROM termine WHERE termine.id = ?';
-    const terminExists = await _db.queryV(stmt, [terminId]);
+    const stmt = 'SELECT count(*) AS count FROM Termine WHERE id = ?';
+    const terminExists = await _db.queryV(stmt, [termin]);
     if (terminExists[0].count === 0) return [];
 
     const role = await _roles.getRoleForTermin(authentication, termin);
@@ -26,17 +26,27 @@ async function setAufstellung(req, authentication) {
         if (boss.aufstellungId == null)
         {
             if (boss.bossId == null) continue;
-            await _termin.addBoss(termin, boss.bossId).then(function(res) {
+            try {
+                let res = await _termin.addBoss(termin, boss.bossId);
                 aufstellung = res.insertId;
-                if (boss.isCM === true || boss.isCM === false) {
+            }
+            catch (e) {
+                //Unable to insert, next boss
+                continue;
+            }
+            if (boss.isCM === true || boss.isCM === false) {
+                try {
                     await _aufstellung.setCM(aufstellung, boss.isCM);
                 }
-            })
+                catch (e) {
+                    //Ignore not possible to set CM.
+                }
+            }
         }
         else
         {
-            const stmt2 = 'SELECT count(*) AS count FROM Aufstellung id = ? and fk_termin = ?';
-            const aufstellungTerminExists = await _db.queryV(stmt2, [aufstellung, terminId]);
+            const stmt2 = 'SELECT count(*) AS count FROM Aufstellung WHERE (id = ?) and (fk_termin = ?)';
+            const aufstellungTerminExists = await _db.queryV(stmt2, [aufstellung, termin]);
             if (aufstellungTerminExists[0].count === 0) continue;
         }
         if (aufstellung == null) continue;
@@ -46,9 +56,19 @@ async function setAufstellung(req, authentication) {
         }
         for (player of boss.positionen) {
             if (1 <= player.position && player.position <= 10) {
-                await _element.setClass(aufstellung, player.position, player.classId);
-                await _element.setRole(aufstellung, player.position, player.roleId);
-                await _element.setName(aufstellung, player.position, player.spielerId);
+                const pos = player.position;
+                const classId = player.classId;
+                const roleId = player.roleId;
+                const playerId = player.spielerId;
+
+                const stmt3 = 'INSERT INTO AufstellungElement (fk_aufstellung, position, fk_class, fk_role, fk_spieler) VALUES (?,?,?,?,?) ON DUPLICATE KEY UPDATE fk_class = ?, fk_role = ?, fk_spieler = ?';
+                try {
+                    await _db.queryV(stmt3, [aufstellung, pos, classId, roleId, playerId, classId, roleId, playerId]);
+                }
+                catch (e) {
+                    // Ignore, next element
+                    continue;
+                }
             }
         }
     }
