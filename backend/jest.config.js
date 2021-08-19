e00be1ffb23bd4a6f6d5bb9e9c1e42ae95a71c01
Kumiko <kumikozelenka@gmail.com>
2021-08-19 19:49:06 +0200

    added .env files.

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -7,6 +7,7 @@ module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
 	moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+	setupFiles: ["./config/setup.ts"],
 	setupFilesAfterEnv: ["jest-extended"],
 	clearMocks: true
 };
