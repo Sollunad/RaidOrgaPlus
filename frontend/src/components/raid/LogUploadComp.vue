2d1d6c6a3a2672314ce8daf1adf7edd42274be27
Kumiko <kumikozelenka@gmail.com>
2021-11-20 14:20:28 +0100

    Fixed the build and replaced the placeholder text

--- frontend/src/components/raid/LogUploadComp.vue
+++ frontend/src/components/raid/LogUploadComp.vue
@@ -3,10 +3,9 @@
 		<h2 class="header">Log Upload</h2>
 		<v-divider class="divider" />
 		<p>
-			Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-			At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-			Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
-			At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+			Für die Seite dps.report gibt es die Möglichkeit, beim hochladen von Logs einen User Token mit anzugeben. Mit diesem User Token, können frühere Logs die auf der Seite mit dem Token hochgeladen wurden, abgefragt werden.
+			RO+ bietet die Möglichkeit an, einen solchen User Token für einen Raid zu hinterlegen oder auch einen automatisch generieren zu lassen, welcher dann auch für die Uploads der Logs entsprechend verwendet wird.
+			Das hinterlegen eines User Tokens in RO+ ist freiwilig, wird aber empfohlen um z.B. später auch direkt über dps.report auf die Logs zugreifen zu können.
 		</p>
 		<v-dialog width="unset" v-model="dialog">
 			<template v-slot:activator="{ on }">
