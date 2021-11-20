529d52d3e976b9fdd5f9b4efb21351b875a2d361
Kumiko <kumikozelenka@gmail.com>
2021-11-20 17:44:35 +0100

    fixed roles not showing in preview

--- backend/endpoints/aufstellungen/element.ts
+++ backend/endpoints/aufstellungen/element.ts
@@ -1,5 +1,5 @@
-import { OkPacket } from 'mysql';
-import * as db from '../../db/connector';
+import { OkPacket } from "mysql";
+import * as db from "../../db/connector";
 import type { element } from "models/Types";
 
 export async function getForTermin(termin: number): Promise<element[]> {
@@ -10,11 +10,11 @@ export async function getForTermin(termin: number): Promise<element[]> {
 		JOIN Spieler ON Spieler.id = AufstellungElement.fk_spieler
 		WHERE Aufstellung.fk_termin = ? FOR UPDATE
 	`;
-    try {
-        return await db.queryV(stmt, termin);
-    } catch(e) {
-        throw e;
-    }
+	try {
+		return await db.queryV(stmt, termin);
+	} catch (e) {
+		throw e;
+	}
 }
 
 export async function getForAufstellung(aufstellung: number): Promise<element[]> {
@@ -25,50 +25,58 @@ export async function getForAufstellung(aufstellung: number): Promise<element[]>
 		JOIN Spieler ON Spieler.id = AufstellungElement.fk_spieler
 		WHERE Aufstellung.id = ? FOR UPDATE
 	`;
-    try {
-        return await db.queryV(stmt, aufstellung);
-    } catch(e) {
-        throw e;
-    }
+	try {
+		return await db.queryV(stmt, aufstellung);
+	} catch (e) {
+		throw e;
+	}
 }
 
 export async function setClass(aufstellung: number, position: number, clss: number): Promise<OkPacket> {
-    const stmt = 'INSERT INTO AufstellungElement (fk_aufstellung, position, fk_class) VALUES (?,?,?) ON DUPLICATE KEY UPDATE fk_class = ?';
-    try {
-        return await db.queryV(stmt, [aufstellung, position, clss, clss]);
-    } catch(e) {
-        throw e;
-    }
+	const stmt =
+		"INSERT INTO AufstellungElement (fk_aufstellung, position, fk_class) VALUES (?,?,?) ON DUPLICATE KEY UPDATE fk_class = ?";
+	try {
+		return await db.queryV(stmt, [aufstellung, position, clss, clss]);
+	} catch (e) {
+		throw e;
+	}
 }
 
 export async function setRole(aufstellung: number, position: number, role: string): Promise<OkPacket> {
-    const stmt = 'INSERT INTO AufstellungElement (fk_aufstellung, position, roles) VALUES (?,?,?) ON DUPLICATE KEY UPDATE roles = ?';
-    try {
-        return await db.queryV(stmt, [aufstellung, position, role, role]);
-    } catch(e) {
-        throw e;
-    }
+	const stmt =
+		"INSERT INTO AufstellungElement (fk_aufstellung, position, roles) VALUES (?,?,?) ON DUPLICATE KEY UPDATE roles = ?";
+	try {
+		return await db.queryV(stmt, [aufstellung, position, role, role]);
+	} catch (e) {
+		throw e;
+	}
 }
 
 export async function setName(aufstellung: number, position: number, name: number): Promise<OkPacket> {
-    const stmt = 'INSERT INTO AufstellungElement (fk_aufstellung, position, fk_spieler) VALUES (?,?,?) ON DUPLICATE KEY UPDATE fk_spieler = ?';
-    try {
-        return await db.queryV(stmt, [aufstellung, position, name, name]);
-    } catch(e) {
-        throw e;
-    }
+	const stmt =
+		"INSERT INTO AufstellungElement (fk_aufstellung, position, fk_spieler) VALUES (?,?,?) ON DUPLICATE KEY UPDATE fk_spieler = ?";
+	try {
+		return await db.queryV(stmt, [aufstellung, position, name, name]);
+	} catch (e) {
+		throw e;
+	}
 }
 
-export async function setCompleteElement(aufstellung: number, position: number, classId: number, roleId: number, playerId: number): Promise<OkPacket> {
-    const stmt = `
+export async function setCompleteElement(
+	aufstellung: number,
+	position: number,
+	classId: number,
+	roleId: number,
+	playerId: number
+): Promise<OkPacket> {
+	const stmt = `
 		INSERT INTO AufstellungElement (fk_aufstellung, position, fk_class, roles, fk_spieler)
 		VALUES (?,?,?,?,?)
 		ON DUPLICATE KEY UPDATE fk_class = ?, roles = ?, fk_spieler = ?
 	`;
-    try {
-        return await db.queryV(stmt, [aufstellung, position, classId, roleId, playerId, classId, roleId, playerId]);
-    }
-    catch (e) {
-        throw e;
-    }
+	try {
+		return await db.queryV(stmt, [aufstellung, position, classId, roleId, playerId, classId, roleId, playerId]);
+	} catch (e) {
+		throw e;
+	}
 }
--- frontend/src/services/endpoints/preview.ts
+++ frontend/src/services/endpoints/preview.ts
@@ -1,30 +1,30 @@
-import con from '../connector';
-import { Aufstellung } from 'models/Aufstellung';
-import { Encounter } from 'models/Encounter';
-import { element, terminDate } from 'models/Types';
+import con from "../connector";
+import { Aufstellung } from "models/Aufstellung";
+import { Encounter } from "models/Encounter";
+import { element, terminDate } from "models/Types";
 
 export default { getAufstellungen, getElements, getRaidName, setPreviewable, getPreviewable, getTerminDate };
 
 async function getAufstellungen(termin: string | number): Promise<(Aufstellung & Encounter)[]> {
-	return await con('preview', 'get', { termin }, false);
+	return await con("preview", "get", { termin }, false);
 }
 
 async function getElements(termin: string | number): Promise<element[]> {
-	return await con('preview/elements', 'get', { termin }, false);
+	return await con("preview/elements", "get", { termin }, false);
 }
 
 async function getRaidName(termin: string | number): Promise<string> {
-	return await con('preview/raid', 'get', { termin }, false);
+	return await con("preview/raid", "get", { termin }, false);
 }
 
 async function setPreviewable(termin: any, able: boolean): Promise<any> {
-	return await con('preview/able', 'post', { termin, able }, true);
+	return await con("preview/able", "post", { termin, able }, true);
 }
 
 async function getPreviewable(termin: any): Promise<any> {
-	return await con('preview/able', 'get', { termin }, true);
+	return await con("preview/able", "get", { termin }, true);
 }
 
 async function getTerminDate(termin: string | number): Promise<terminDate> {
-	return await con('preview/date', 'get', { termin }, false);
+	return await con("preview/date", "get", { termin }, false);
 }
