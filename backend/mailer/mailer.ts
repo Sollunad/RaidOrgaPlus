0866086ebefe150de3a5666df09039493a969f34
Kumiko <kumikozelenka@gmail.com>
2022-03-23 19:21:32 +0100

    fixed (?) the mailer for reseting the password

--- backend/mailer/mailer.ts
+++ backend/mailer/mailer.ts
@@ -5,32 +5,45 @@ import config from "./config.json";
 export { sendPasswortResetMail as passwortReset };
 
 function sendEmail(user: Spieler, subject: string, text: string): void {
-    const transporter = nodemailer.createTransport(config);
-    const to = user.email;
+	const mailAdress = "raidorgakontakt@gmail.com";
+	const transporter = nodemailer.createTransport({
+		host: "smtp.gmail.com",
+		secure: true,
+		auth: {
+			type: "OAuth2",
+			user: mailAdress,
+			clientId: config.clientId,
+			clientSecret: config.clientSecrect,
+			refreshToken: config.refreshToken,
+		},
+	} as any);
+	const to = user.email;
 
-    const mail = {
-        from: {
-            name: 'RaidOrga+',
-            address: 'raidorgakontakt@gmail.com',
-        },
-        to, subject, text
-    };
+	const mail = {
+		from: {
+			name: "RaidOrga+",
+			address: mailAdress,
+		},
+		to,
+		subject,
+		text,
+	};
 
-    transporter.sendMail(mail);
+	transporter.sendMail(mail);
 }
 
 function sendPasswortResetMail(user: Spieler, token: string): void {
-    const subject = 'Passwort-Reset angefordert';
-    const link = `https://orga.rising-light.de/#/reset/${token}`;
-    const text =
-        `Hallo ${user.name},` +
-        `\nfür deinen Account mit dem Accountnamen ${user.accname} wurde ein Passwort-Reset angefordert. Unter folgendem Link kannst du dein Passwort neu setzen:` +
-        `\n` +
-        `\n${link}` +
-        `\n` +
-        `\nDer Link ist gültig für 24 Stunden. Falls du keinen Password-Reset angefordert hast, ignoriere diese Mail einfach.` +
-        `\n` +
-        `\nViele Grüße` +
-        `\nDas Leitungsteam von Rising-Light.`;
-    sendEmail(user, subject, text);
+	const subject = "Passwort-Reset angefordert";
+	const link = `https://orga.rising-light.de/#/reset/${token}`;
+	const text =
+		`Hallo ${user.name},` +
+		`\nfür deinen Account mit dem Accountnamen ${user.accname} wurde ein Passwort-Reset angefordert. Unter folgendem Link kannst du dein Passwort neu setzen:` +
+		`\n` +
+		`\n${link}` +
+		`\n` +
+		`\nDer Link ist gültig für 24 Stunden. Falls du keinen Password-Reset angefordert hast, ignoriere diese Mail einfach.` +
+		`\n` +
+		`\nViele Grüße` +
+		`\nDas Leitungsteam von Rising-Light.`;
+	sendEmail(user, subject, text);
 }
