a9b5e253d3f30a67a1f0c95b970c98a4074b5db2
Kumiko <kumikozelenka@gmail.com>
2022-03-28 23:06:04 +0200

    changed the document in the help page.

--- frontend/src/pages/HelpPage.vue
+++ frontend/src/pages/HelpPage.vue
@@ -1,25 +1,28 @@
 <template>
-    <div class="container">
-        <iframe src="https://docs.google.com/document/d/e/2PACX-1vTnQf-KGAqxGknr1tBa4cbOEUhjguJTf6ZoTGekc2tUjj7DPcLVWa4Et16AST_SsV9UDlcVO57ZtXTH/pub?embedded=true" class="iframe"></iframe>
-    </div>
+	<div class="container">
+		<iframe
+			src="https://docs.google.com/document/d/e/2PACX-1vQdk_8rIq7oIZEqA7mNKfm2vk1Ayhgj43x5OYsz0XB9-1zm7zCmNLMUOE8Ws8NGhjmztixfHgjqYl02/pub?embedded=true"
+			class="iframe"
+		></iframe>
+	</div>
 </template>
 
 <script lang="ts">
-	import Vue from 'vue';
+	import Vue from "vue";
 
-    export default Vue.extend({
-        name: "HelpPage"
-    })
+	export default Vue.extend({
+		name: "HelpPage",
+	});
 </script>
 
 <style scoped>
-    .container {
-        display: flex;
-        width: inherit;
-        height: 95%;
-    }
+	.container {
+		display: flex;
+		width: inherit;
+		height: 95%;
+	}
 
-    .iframe {
-        flex-grow: 1;
-    }
+	.iframe {
+		flex-grow: 1;
+	}
 </style>
