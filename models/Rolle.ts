3e529a6d740cfba6fd604b64fd459399087a0e8a
Kumiko <kumikozelenka@gmail.com>
2022-05-23 18:03:44 +0200

    #5 added the missing supp-icons for the templates

--- models/Rolle.ts
+++ models/Rolle.ts
@@ -11,8 +11,8 @@ export const ROLES: Role[] = [
 	{ id: 4, name: 'Healer', abbr: 'H' },
 	{ id: 5, name: 'Support', abbr: 'U' },
 	{ id: 6, name: 'Banner Slave', abbr: 'B' },
-	{ id: 7, name: 'Special', abbr: 'S' },
-	{ id: 8, name: 'Kiter', abbr: 'K' },
 	{ id: 9, name: 'Quickness', abbr: 'Q' },
 	{ id: 10, name: 'Alacrity', abbr: 'A' },
+	{ id: 8, name: 'Kiter', abbr: 'K' },
+	{ id: 7, name: 'Special', abbr: 'S' },
 ];
